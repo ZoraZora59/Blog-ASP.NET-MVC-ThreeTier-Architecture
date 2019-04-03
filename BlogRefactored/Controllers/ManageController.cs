90a4de5de2f1044f102dde06ff1c4095ae74476a
ZoraGK <jyh199759@126.com>
2019-04-03 18:07:14 +0800

    将Form模式改为Ajax

--- BlogRefactored/Controllers/ManageController.cs
+++ BlogRefactored/Controllers/ManageController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
