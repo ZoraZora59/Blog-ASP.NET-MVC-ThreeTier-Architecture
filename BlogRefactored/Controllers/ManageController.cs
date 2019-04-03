0a3a3028e3c2d9d26efe680039d18f95101825a7
ZoraGK <jyh199759@126.com>
2019-04-03 16:42:33 +0800

    添加Kindeditor上传方法

--- BlogRefactored/Controllers/ManageController.cs
+++ BlogRefactored/Controllers/ManageController.cs
@@ -278,5 +278,78 @@ namespace BlogRefactored.Controllers
 			return Json(0);
 		}
 		#endregion
+
+		#region Kindeditor上传
+		public ActionResult Upload()
+		{
+			//文件保存目录路径
+			String savePath = "/attached/";
+			//文件保存目录URL
+			String saveUrl = "/attached/";
+			//定义允许上传的文件扩展名
+			Hashtable extTable = new Hashtable
+			{
+				{ "image", "gif,jpg,jpeg,png,bmp" },
+				{ "flash", "swf,flv" },
+				{ "media", "swf,flv,mp3,wav,wma,wmv,mid,avi,mpg,asf,rm,rmvb" },
+				{ "file", "doc,docx,xls,xlsx,ppt,htm,html,txt,zip,rar,gz,bz2" }
+			};
+			//最大文件大小
+			int maxSize = 1000000;
+			HttpPostedFileBase imgFile = Request.Files["imgFile"];
+			if (imgFile == null)
+			{
+				return Content("请选择文件。");
+			}
+			String dirPath = Server.MapPath(savePath);
+			if (!Directory.Exists(dirPath))
+			{
+				return Content("上传目录不存在。");
+			}
+			String dirName = Request.QueryString["dir"];
+			if (String.IsNullOrEmpty(dirName))
+			{
+				dirName = "image";
+			}
+			if (!extTable.ContainsKey(dirName))
+			{
+				return Content("目录名不正确。");
+			}
+			String fileName = imgFile.FileName;
+			String fileExt = Path.GetExtension(fileName).ToLower();
+			if (imgFile.InputStream == null || imgFile.InputStream.Length > maxSize)
+			{
+				return Content("上传文件大小超过限制。");
+			}
+			if (String.IsNullOrEmpty(fileExt) || Array.IndexOf(((String)extTable[dirName]).Split(','), fileExt.Substring(1).ToLower()) == -1)
+			{
+				return Content("上传文件扩展名是不允许的扩展名。\n只允许" + ((String)extTable[dirName]) + "格式。");
+			}
+			//创建文件夹
+			dirPath += dirName + "/";
+			saveUrl += dirName + "/";
+			if (!Directory.Exists(dirPath))
+			{
+				Directory.CreateDirectory(dirPath);
+			}
+			String ymd = DateTime.Now.ToString("yyyyMMdd", DateTimeFormatInfo.InvariantInfo);
+			dirPath += ymd + "/";
+			saveUrl += ymd + "/";
+			if (!Directory.Exists(dirPath))
+			{
+				Directory.CreateDirectory(dirPath);
+			}
+			String newFileName = DateTime.Now.ToString("yyyyMMddHHmmss_ffff", DateTimeFormatInfo.InvariantInfo) + fileExt;
+			String filePath = dirPath + newFileName;
+			imgFile.SaveAs(filePath);
+			String fileUrl = saveUrl + newFileName;
+			Hashtable hash = new Hashtable
+			{
+				["error"] = 0,
+				["url"] = fileUrl
+			};
+			return Json(hash);
+		}
+		#endregion
 	}
 }
